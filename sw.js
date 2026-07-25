4dee77b7067f06efe66e06c100a102b8b799bfed
elsayedb1318-star <elsayedb1318@gmail.com>
2026-07-25 20:10:40 +0300

    Implement basic service worker for PWA
    Add a simple service worker to enable PWA installation

---
+++ sw.js
@@ -0,0 +1,14 @@
+// Service worker بسيط عشان يخلي الموقع قابل للتثبيت كـ PWA
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
